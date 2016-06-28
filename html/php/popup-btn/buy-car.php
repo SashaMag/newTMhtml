21aab3c6c738b337d162842670a244591c40e27a
SashaMag <spabb@yandex.ru>
2016-06-28 03:14:43 +0100

    fix: display none - put to css ( sass )

--- html/php/popup-btn/buy-car.php
+++ html/php/popup-btn/buy-car.php
@@ -2,7 +2,7 @@
 <a href="" class="trigger" data-popup="buy-car">Купить авто</a>
 
 
-<div class="modal" id="popup-buy-car" style="display: none">
+<div class="modal" id="popup-buy-car">
 
   <?php
 	include $_SERVER['DOCUMENT_ROOT'].'/html/php/forms/form-buy.php'
