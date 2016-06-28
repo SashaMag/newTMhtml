be6f41363bac8677dcd6ab8f00d97b6813aa15bb
SashaMag <spabb@yandex.ru>
2016-06-28 09:22:20 +0100

    fix: display none - put to css ( sass )

--- html/php/popup-btn/buy-car.php
+++ html/php/popup-btn/buy-car.php
@@ -1,11 +1,13 @@
 
 <a href="" class="trigger" data-popup="buy-car">Купить авто</a>
 
-
-<div class="modal" id="popup-buy-car">
+<!-- тут вообще странно, если убрать D:N -->
+<!--noindex-->
+<div class="modal" id="popup-buy-car" style="display: none">
 
   <?php
 	include $_SERVER['DOCUMENT_ROOT'].'/html/php/forms/form-buy.php'
 	?>
 
 </div>
+<!--/noindex-->
