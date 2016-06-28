42026fde2427020381b59c3a73d53c2704bfd894
SashaMag <spabb@yandex.ru>
2016-06-28 10:26:40 +0100

    fix: display none - /css/sass/...css !!!

--- html/php/head.php
+++ html/php/head.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="/css/application.css">
+<link rel="stylesheet" href="/css/sass/application.css">
 <link rel="stylesheet" href="/css/additional-style.css">
 <link rel="stylesheet" href="/fonts/fonts.css">
 
