c1a2b6a6eec2578d6b0e3ebc7fea7e4492a782d7
SashaMag <spabb@yandex.ru>
2016-06-28 03:37:07 +0100

    fix: display none - put to css ( sass )

--- html/php/popup-btn/credit-calc.php
+++ html/php/popup-btn/credit-calc.php
@@ -2,7 +2,7 @@
 <a href="" class="trigger" data-popup="credit-calc">Кредитный калькулятор</a>
 
 
-<div class="modal" id="popup-credit-calc" style="display: none">
+<div class="modal" id="popup-credit-calc">
 
   <?php
 	include $_SERVER['DOCUMENT_ROOT'].'/html/php/forms/form-credit-calc.php'
